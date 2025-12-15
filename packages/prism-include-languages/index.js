7736df103491e793717c0a221fd2225a3f362967
AugChao <august.chao@twsc.io>
2025-12-15 16:51:55 +0800

    fix(ci): provide @theme/prism-include-languages stub package

---
+++ packages/prism-include-languages/index.js
@@ -0,0 +1,4 @@
+export default function prismIncludeLanguages() {
+  // Docusaurus expects this module to exist at build/SSG time.
+  // In this site we don't need to dynamically extend Prism languages.
+}
